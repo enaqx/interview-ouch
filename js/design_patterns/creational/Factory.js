d809179615bebcfb057a962bfe2957be843906ed
Nick Raienko <enaqxx@gmail.com>
2015-06-08 14:26:15 +0300

    Indent using spaces

--- js/design_patterns/creational/Factory.js
+++ js/design_patterns/creational/Factory.js
@@ -5,31 +5,31 @@
 
 /* Declaration of Factory with Encapsulation */
 function Factory(name) {
-	var runStr = ' is running'; // private value
+  var runStr = ' is running'; // private value
 
-	return {
-		run: function() { // public method
-			console.log(name + runStr);
-		}
-	};
+  return {
+    run: function() { // public method
+      console.log(name + runStr);
+    }
+  };
 };
 
 
 /* Factory Inheritance with Mutating */
 function Manufactory(name) {
-	var factoryObj = Factory(name); // inheritance
+  var factoryObj = Factory(name); // inheritance
 
-	var grindStr = ' is grinding'; 	// private value
-	var grinding = function() {			// private method
-		console.log(name + grindStr);
-	}
+  var grindStr = ' is grinding';  // private value
+  var grinding = function() {     // private method
+    console.log(name + grindStr);
+  }
 
-	factoryObj.grind = function() { // public method
-		this.run();
-		grinding();
-	};
+  factoryObj.grind = function() { // public method
+    this.run();
+    grinding();
+  };
 
-	return factoryObj;
+  return factoryObj;
 };
 
 var factory = Manufactory('Mill');
