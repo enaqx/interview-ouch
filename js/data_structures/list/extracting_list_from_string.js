474954ec0168edc8de42ed77d29774671a1346f5
Nick Raienko <enaqxx@gmail.com>
2015-06-08 21:26:23 +0300

    Add Extracting list from string example

---
+++ js/data_structures/list/extracting_list_from_string.js
@@ -0,0 +1,22 @@
+/**
+ * Question: Extract List from a string.
+ */
+
+
+/* Given the string with fruits list. */
+var sentence = 'This is one sentence. This is a sentence with a list of items:'
+	+ 'cherries, oranges, apples, bananas. That was the list of items.';
+
+/* Find borders of friuts list within given string. */
+var startOfList = sentence.indexOf(':');
+var endOfList = sentence.indexOf('.', startOfList + 1);
+
+/* Extract list from given borders. */
+var fruitString = sentence.substring(startOfList + 1, endOfList);
+var fruits = fruitString.split(',');
+
+fruits.forEach(function(element, index, array) {
+	array[index] = element.trim();
+});
+
+console.log(fruits);
