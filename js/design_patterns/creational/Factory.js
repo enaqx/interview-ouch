2e0e9e67aa1e431a3193fd74c45bcd9cc3fd1b10
Nick Raienko <enaqxx@gmail.com>
2015-06-08 14:27:14 +0300

    Update styling

--- js/design_patterns/creational/Factory.js
+++ js/design_patterns/creational/Factory.js
@@ -5,10 +5,10 @@
 
 /* Declaration of Factory with Encapsulation */
 function Factory(name) {
-  var runStr = ' is running'; // private value
+  var runStr = ' is running';     // private value
 
   return {
-    run: function() { // public method
+    run: function() {             // public method
       console.log(name + runStr);
     }
   };
