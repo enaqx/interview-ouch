a4ae3bb901e55322a4795e2c69606c6bd5312299
Nick Raienko <enaqxx@gmail.com>
2015-06-10 22:01:27 +0300

    Add capitalization of first letter in name example

---
+++ js/data_structures/strings/capitalize_first_letter.js
@@ -0,0 +1,11 @@
+/**
+ * Question: Capitalize fist letter in name.
+ */
+
+
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+}
+
+console.log('JANE'.capitalize());
+console.log('john'.capitalize());
